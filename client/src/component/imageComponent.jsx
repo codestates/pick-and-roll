91f10a4aff85c548a047befbb02bbce205632c60
wonjae jun <woncha7764@gmail.com>
2021-10-13 12:25:17 +0900

    검색사진 및 별점 2개

--- client/src/component/imageComponent.jsx
+++ client/src/component/imageComponent.jsx
@@ -10,13 +10,13 @@ const ImageComponent = ({ url, info }) => {
     userId,
     title,
     introduction,
+    userNickName,
     category,
     mainImg,
     tasteAvg,
     easyAvg,
     createdAt,
   } = info
-  const totalScore = (tasteAvg + easyAvg) / 2
 
   const toPost = () => {
     history.push(`/recipe/id=${id}`)
@@ -26,11 +26,15 @@ const ImageComponent = ({ url, info }) => {
     <>
       <Background className="back" onClick={toPost}>
         <ScoreWrap>
-          <span className="score">{totalScore}</span>
+          <span className="score">간편성: &nbsp;{easyAvg}</span>
+          <ImStarFull className="icon" />
+        </ScoreWrap>
+        <ScoreWrap>
+          <span className="score">맛: &nbsp;{tasteAvg}</span>
           <ImStarFull className="icon" />
         </ScoreWrap>
         <p className="title">{title}</p>
-        <div className="editor">작성자 : {userId}</div>
+        <div className="editor">작성자 : {userNickName}</div>
         <p className="date">게시일 : {createdAt.substring(0, 10)}</p>
         <div className="instruction">{introduction}</div>
       </Background>
@@ -79,9 +83,6 @@ const Background = styled.div`
     font-weight: bold;
     padding-bottom: 15px;
   }
-  .icon {
-    color: rgb(247, 215, 36);
-  }
   .editor {
     color: white;
   }
@@ -103,8 +104,12 @@ const Background = styled.div`
 `
 
 const ScoreWrap = styled.div`
-  position: absolute;
+  position: relative;
   top: 20px;
   right: 16px;
+  .icon {
+    color: rgb(247, 215, 36);
+    margin-left: 100%;
+  }
 `
 export default ImageComponent
